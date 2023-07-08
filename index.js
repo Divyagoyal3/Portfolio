8610730a71a23a9843039e8b45dc78e5dc7d3f66
Divyagoyal3 <Divyagoyal3>
2023-07-08 21:32:20 +0530

    animation added

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-const cursor = document.querySelector(".cursor");
-var timeout;
+var cursor = document.querySelector(".cursor");
+
 //cursor movement
 
-document.addEventListener("mousemove", (e) => {
-  let x = e.pageX;
-  let y = e.pageY;
-
-  cursor.style.top = y + "px";
-  cursor.style.left = x + "px";
+document.onmousemove =function(e){
+  cursor.style.top = (e.pageY - 25) + "px";
+  cursor.style.left = (e.pageX - 25) + "px";
   cursor.style.display = "block";
+}
 
-  //function animation
-  function mousestopped() {
-    cursor.style.display = "none";
-  }
-  //hode animation
-  clearTimeout(timeout);
-  timeout = setTimeout(mousestopped, 1000000)
-});
-
-//stop animation off the screen
- document.addEventListener("mouseout", () =>{
-    cursor.style.display = 'none';
- });
 
  //gsap
  gsap.from(".bar abbr",{
